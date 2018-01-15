4daa6a0a044337ea968ce913b7dac8ba469309f8
Ben Purslow <ben.purslow@tangent.co.uk>
2018-01-15 16:49:13 +0000

    Setup api mock data. Generic component setup in t1 to handle said data

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var data = require('../api/data.json');
 
 /* GET home page. */
 router.get('/t1', function(req, res, next) {
-  res.render('t1/index', { title: 'Express' });
+  res.render('t1/index', data);
 });
 
 module.exports = router;
--- src/t1/js/entry.js
+++ src/t1/js/entry.js
@@ -1,7 +1,6 @@
 import {ready} from '../../global/js/utils';
 import '../styles/t1.scss';
 
-
 const init = () => {
   const first = document.querySelector('.first');
   const second = document.querySelector('.second');
